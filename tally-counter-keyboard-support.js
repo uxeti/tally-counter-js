455f4476ee67d6918b42033990a8eaaba39b22fe
me <61819696+uxeti@users.noreply.github.com>
2020-03-06 04:52:27 +0900

    Added keyboard shortcuts

---
+++ tally-counter-keyboard-support.js
@@ -0,0 +1,23 @@
+function CountUpAllCountersStartWith(lowercaseLetter) {
+    let counterNames = document.getElementsByClassName("tally-counter-name")
+
+    for (var i = 0; i < counterNames.length; i++) {
+        let counterName = counterNames[i]
+        let counterNameLowerCase = counterName.value.toLowerCase()
+        if (counterNameLowerCase[0] == lowercaseLetter[0]) {
+            counter = GetTallyCounterElement(counterName)
+            ChangeCount(counter, 1)
+        }
+    }
+}
+
+function OnKeyDown(event) {
+    if (event.target.tagName != "INPUT") {
+        let lowercaseLetter = event.key.toLowerCase()
+        if (lowercaseLetter.length == 1) {
+            CountUpAllCountersStartWith(lowercaseLetter)
+        }
+    }
+}
+
+document.addEventListener('keydown', OnKeyDown);
